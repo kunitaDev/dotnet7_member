07a648c94b79c0d378b00d63bbe9c5a9f8c56b47
kunitaDev <kunita.boonyong@gmail.com>
2022-12-07 13:05:56 +0700

    Add project files.

---
+++ dotnet7_member/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnet7_member.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
